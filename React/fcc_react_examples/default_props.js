7e1b30de40a66a3ca9a272f320af29eacf2fd001
uaaeu <yurtmusa@gmail.com>
2020-07-02 22:52:26 +0300

    overrride default props

--- React/fcc_react_examples/default_props.js
+++ React/fcc_react_examples/default_props.js
@@ -8,3 +8,23 @@ const ShoppingCart = (props) => {
 };
 // Make default props of ShoppingCart to items: 0
 ShoppingCart.defaultProps = { items: 0 };
+
+//Override Default Props
+const Items = (props) => {
+  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>;
+};
+
+// declare defaultProps of Items props quantity 
+Items.defaultProps = {
+  quantity: 0,
+};
+
+class ShoppingCart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    // default props overrided
+    return <Items quantity={10} />;
+  }
+}
