8dc1714d8a8961f8b8c8184bddcd574c72354b9a
uaaeu <yurtmusa@gmail.com>
2020-07-02 19:31:57 +0300

    Compose React Components

---
+++ React/fcc_react_examples/nested_components.js
@@ -0,0 +1,31 @@
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+        {/* Nest two components*/}
+        <NonCitrus />
+        <Citrus />
+      </div>
+    );
+  }
+}
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        {/* Nest two components and one of them already have two nested */}
+        <Fruits />
+        <Vegetables />
+      </div>
+    );
+  }
+}
