7ff2da56ea93c7ac041ae3ab04faa234ba2a9b4a
uaaeu <yurtmusa@gmail.com>
2020-07-02 14:41:31 +0300

    react app and component created

---
+++ React/fcc_react/src/components/Exercises.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Exercises extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello JSX Exercises!</h1>
+            </div>
+        )
+    }
+}
+
+export default Exercises
