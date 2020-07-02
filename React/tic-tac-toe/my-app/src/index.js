4297e13c5e8846a772e0af8aa1d1b47f14dcdddf
uaaeu <yurtmusa@gmail.com>
2020-07-02 11:45:26 +0300

    alert adder when clicked

--- React/tic-tac-toe/my-app/src/index.js
+++ React/tic-tac-toe/my-app/src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          alert("click");
+        }}
+      >
+        {this.props.value}
+      </button>
+    );
   }
 }
 
