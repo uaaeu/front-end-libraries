ab39427069fd8065a4c80c563eade36ce1f0d43d
uaaeu <yurtmusa@gmail.com>
2020-07-02 18:16:24 +0300

    Render a Class Component to the DOM

--- React/fcc_react/src/index.js
+++ React/fcc_react/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// Render a Class Component to the DOM
 ReactDOM.render(
   <React.StrictMode>
     <App />
