1c14aca705e0018d93403cc244f2b7339bece3bf
uaaeu <yurtmusa@gmail.com>
2020-07-02 15:14:28 +0300

    Define an HTML Class in JSX

--- React/fcc_react/src/components/Exercises.jsx
+++ React/fcc_react/src/components/Exercises.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class Exercises extends Component {
   render() {
     return (
-      <div>{/* comment for div */}
+      <div className="myDiv">
         <h1>Morning Sunshine</h1>
         <p>From orange sunshine to dark color revel</p>
         <ul>
