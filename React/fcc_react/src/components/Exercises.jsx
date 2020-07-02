8c5734871acbc4788372610b686c347251bd5387
uaaeu <yurtmusa@gmail.com>
2020-07-02 15:28:36 +0300

    Stateless Functional Component

--- React/fcc_react/src/components/Exercises.jsx
+++ React/fcc_react/src/components/Exercises.jsx
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 
 export class Exercises extends Component {
   render() {
+    const MyComponent = () => { {/* Stateless Functional Component */}
+      return <div>Because you awaken...</div>;
+    };
     return (
-      <div className="myDiv"> {/* Define an HTML Class in JSX */}
+      <div className="myDiv">
+        {" "}
+        {/* Define an HTML Class in JSX */}
         <h1>Morning Sunshine</h1>
         <p>From orange sunshine to dark color revel</p>
         <ul>
@@ -12,7 +17,8 @@ export class Exercises extends Component {
           <li>Mimosa Hostilis</li>
         </ul>
         <h3>Night night dear everything...</h3>
-        <hr />{/* Self-Closing JSX Tags */}
+        <hr /> {/* Self-Closing JSX Tags */}
+        <MyComponent />
       </div>
     );
   }
