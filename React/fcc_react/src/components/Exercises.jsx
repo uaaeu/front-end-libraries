440a44a9cb887a4c45884452306fd0cb489bcb7f
uaaeu <yurtmusa@gmail.com>
2020-07-02 14:55:40 +0300

    Add Comments in JSX

--- React/fcc_react/src/components/Exercises.jsx
+++ React/fcc_react/src/components/Exercises.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export class Exercises extends Component {
   render() {
     return (
-      <div>
+      <div>{/* comment for div */}
         <h1>Morning Sunshine</h1>
         <p>From orange sunshine to dark color revel</p>
         <ul>
