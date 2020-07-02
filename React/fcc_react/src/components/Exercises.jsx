4199e265e0fab698a358c57061426c89b47fbcf5
uaaeu <yurtmusa@gmail.com>
2020-07-02 16:27:06 +0300

    Render Nested Components

--- React/fcc_react/src/components/Exercises.jsx
+++ React/fcc_react/src/components/Exercises.jsx
@@ -1,25 +1,41 @@
 import React, { Component } from "react";
 
+// Component with Composition
+// Stateless Functional Component
+const FooterComp = () => {
+  return (
+    <div>
+      <h4>Because you awaken...</h4>
+      <p>...from the biggest dream ever!</p>
+    </div>
+  );
+};
+
+// Render Nested Components
+const BodyComp = () => {
+  return (
+    <div>
+      <p>From orange sunshine to dark color revel</p>
+      <ul>
+        <li>Psilocybin</li>
+        <li>Ergotamine</li>
+        <li>Mimosa Hostilis</li>
+      </ul>
+      <h3>Night night dear everything...</h3>
+      <hr /> {/* Self-Closing JSX Tags */}
+    </div>
+  );
+};
+
 // React Component
 export class Exercises extends Component {
   render() {
-    // Stateless Functional Component
-    const MyComponent = () => {
-      return <div>Because you awaken...</div>;
-    };
     return (
-      <div className="myDiv">
-        {/* Define an HTML Class in JSX */}
+      // Define an HTML Class in JSX
+      <div className="HeaderComp">
         <h1>Morning Sunshine</h1>
-        <p>From orange sunshine to dark color revel</p>
-        <ul>
-          <li>Psilocybin</li>
-          <li>Ergotamine</li>
-          <li>Mimosa Hostilis</li>
-        </ul>
-        <h3>Night night dear everything...</h3>
-        <hr /> {/* Self-Closing JSX Tags */}
-        <MyComponent />
+        <BodyComp />
+        <FooterComp />
       </div>
     );
   }
