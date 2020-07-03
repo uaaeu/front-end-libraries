da7434419c723a3ec358e404a395f300def9a4da
uaaeu <yurtmusa@gmail.com>
2020-07-03 10:56:59 +0300

    Return state with the variable that created

--- React/fcc_react_examples/state.js
+++ React/fcc_react_examples/state.js
@@ -4,13 +4,18 @@ class StatefulComponent extends React.Component {
     // Create a Stateful Component
     this.state = {
       name: "Name",
+      surname: "Surname",
     };
   }
   render() {
+    // Create a variable to render a state
+    const surname = this.state.surname;
     return (
       <div>
-      {/* Return state with this.state */}
+        {/* Return state with this.state */}
         <h1>{this.state.name}</h1>
+        {/* Return state with the variable that created */}
+        <h1>{surname}</h1>
       </div>
     );
   }
