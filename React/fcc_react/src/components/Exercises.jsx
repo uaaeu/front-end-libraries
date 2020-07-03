9da901475d1a6988650ada9a364b338217370bc5
uaaeu <yurtmusa@gmail.com>
2020-07-03 11:59:21 +0300

    constructor, handleClick, setState, button

--- React/fcc_react/src/components/Exercises.jsx
+++ React/fcc_react/src/components/Exercises.jsx
@@ -29,13 +29,32 @@ const BodyComp = () => {
 
 // React Component
 export class Exercises extends Component {
+  // Constructor created for states
+  constructor(props) {
+    super(props);
+    // Created a state named bicycleDay
+    this.state = {
+      bicycleDay: 'Bicycle Day'
+    }
+    // Created a handleClick to bind click
+    this.handleClick = this.handleClick.bind(this);
+  }
+  // A handleClick function to set state of bicycleDay
+  handleClick() {
+    this.setState({
+      bicycleDay: 'April 19, 1943'
+    })
+  }
   render() {
     return (
       // Define an HTML Class in JSX
       <div className="HeaderComp">
         <h1>Morning Glory</h1>
         <BodyComp />
-        <FooterComp date="November 16 1938"/>
+        <FooterComp date="November 16, 1938"/>
+        {/* Create a button with onClick handler to change button name while clicked */}
+        <p>made</p>
+        <button onClick={this.handleClick}>{this.state.bicycleDay}</button>
       </div>
     );
   }
