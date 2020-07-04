1f558ed294c614c71bacd9e69f51be26115f8d7e
uaaeu <yurtmusa@gmail.com>
2020-07-04 15:01:48 +0300

    Use State to Toggle an Element

---
+++ React/fcc_react_examples/state_toggle.js
@@ -0,0 +1,32 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false,
+    };
+    // Create toggleVisibility and bind this to action 
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+  // Create toggleVisibility function and use state to toggle visibility element and change it
+  toggleVisibility() {
+    this.setState((state) => ({
+      visibility: !state.visibility,
+    }));
+  }
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+}
