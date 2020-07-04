3d40c9ddab4801f54461b1d6b799ed38e4caf61d
uaaeu <yurtmusa@gmail.com>
2020-07-04 18:39:13 +0300

    reset input after submit

--- React/fcc_react/src/components/MyForm.jsx
+++ React/fcc_react/src/components/MyForm.jsx
@@ -4,7 +4,7 @@ export class MyForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "add a note...",
+      input: "",
       submit: "",
     };
     this.handleChange = this.handleChange.bind(this);
@@ -19,6 +19,7 @@ export class MyForm extends React.Component {
     event.preventDefault();
     this.setState({
       submit: this.state.input,
+      input: ''
     });
   }
   render() {
@@ -27,7 +28,7 @@ export class MyForm extends React.Component {
         <h1>{this.state.submit}</h1>
         <form onSubmit={this.handleSubmit}>
           <input value={this.state.input} onChange={this.handleChange} />
-          <button type="submit">Submit!</button>
+          <button type="submit">Add Note</button>
         </form>
       </div>
     );
