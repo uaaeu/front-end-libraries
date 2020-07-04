4f9281aed071b10511a558ec1d28497e0f3942ac
uaaeu <yurtmusa@gmail.com>
2020-07-04 17:02:16 +0300

    Pass a Callback as Props

---
+++ React/fcc_react_examples/callback_as_props.js
@@ -0,0 +1,57 @@
+// Pass a Callback as Props
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  }
+  render() {
+    return (
+      <div>
+        {/* Render GetInput and RenderInput as child */}
+        {/* Pass a Callback and state as props to GetInput component  */}
+        <GetInput
+          input={this.state.inputValue}
+          handleChange={this.handleChange}
+        />
+        {/* Pass state as props */}
+        <RenderInput input={this.state.inputValue} />
+      </div>
+    );
+  }
+}
+
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input value={this.props.input} onChange={this.props.handleChange} />
+      </div>
+    );
+  }
+}
+
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+}
