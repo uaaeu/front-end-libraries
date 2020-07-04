e5884260d63229ffa42d2467e1fbe2b8ee2a06fd
uaaeu <yurtmusa@gmail.com>
2020-07-04 17:19:45 +0300

    componenetDidMount Lifecycle Method

--- React/fcc_react_examples/lifecycle.js
+++ React/fcc_react_examples/lifecycle.js
@@ -1,13 +1,38 @@
-// Use the Lifecycle Method
+// Use the Lifecycle Method componenetWillMount
 class MyComponent extends React.Component {
   constructor(props) {
     super(props);
   }
-// componenetWillMount method to catch components at certain points in time.
+  // componenetWillMount method to catch components at certain points in time.
   componentWillMount() {
     console.log("test");
   }
   render() {
     return <div />;
+  }
+}
+
+// Use the Lifecycle Method componenetDidMount
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeUsers: null,
+    };
+  }
+  // componenetDidMount method is called after a component is mounted to the DOM.
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        activeUsers: 1273,
+      });
+    }, 2500);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Active Users: {this.state.activeUsers}</h1>
+      </div>
+    );
   }
 }
