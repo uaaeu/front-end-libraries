a3c569228b618d0d3cea96d19b4317b51e645ffd
uaaeu <yurtmusa@gmail.com>
2020-07-04 22:42:27 +0300

    Change Inline CSS Conditionally

--- React/fcc_react_examples/inline_styles.js
+++ React/fcc_react_examples/inline_styles.js
@@ -12,3 +12,37 @@ class Colorful extends React.Component {
     return <div style={styles}>Style Me!</div>;
   }
 }
+
+// Change Inline CSS Conditionally Based on Component State
+class GateKeeper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
+  render() {
+    let inputStyle = {
+      border: "1px solid black",
+    };
+    // Change Inline CSS Conditionally Based on Component State
+    if (this.state.input.length > 15) {
+      inputStyle.border = "3px solid red";
+    }
+    return (
+      <div>
+        <h3>Don't Type Too Much:</h3>
+        <input
+          type="text"
+          style={inputStyle}
+          value={this.state.input}
+          onChange={this.handleChange}
+        />
+      </div>
+    );
+  }
+}
