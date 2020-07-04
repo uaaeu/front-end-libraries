d8ee982b8236a15afb057017efb9694e3913b0c0
uaaeu <yurtmusa@gmail.com>
2020-07-04 18:32:26 +0300

    Add styles with a variable that defined first

--- React/fcc_react_examples/inline_styles.js
+++ React/fcc_react_examples/inline_styles.js
@@ -4,3 +4,11 @@ class Colorful extends React.Component {
     return <div style={{ color: "red", fontSize: "72px" }}>Big Red</div>;
   }
 }
+
+// Add styles with a variable that defined first
+const styles = { color: "purple", fontSize: 40, border: "2px solid purple" };
+class Colorful extends React.Component {
+  render() {
+    return <div style={styles}>Style Me!</div>;
+  }
+}
