1eacfaa0e5a5e5db4d080706ab8b91e861e4c917
uaaeu <yurtmusa@gmail.com>
2020-07-05 16:52:02 +0300

    Remove an Item from an Array

--- Redux/immutability.js
+++ Redux/immutability.js
@@ -13,7 +13,7 @@ const immutableReducer = (state = todos, action) => {
   switch (action.type) {
     case ADD_TO_DO:
       // don't mutate state
-      return [...todos, action.todo];
+      return [...todos, action.todo]; // todos.concat(action.todo) will work too
     default:
       return state;
   }
@@ -23,6 +23,25 @@ const addToDo = (todo) => {
   return {
     type: ADD_TO_DO,
     todo,
+  };
+};
+
+const store = Redux.createStore(immutableReducer);
+
+// Remove an Item from an Array
+const immutableReducer = (state = [0, 1, 2, 3, 4, 5], action) => {
+  switch (action.type) {
+    case "REMOVE_ITEM":
+      return state.slice(0, action.index).concat(state.slice(action.index + 1)); // or [...state.slice(0, action.index), ...state.slice(action.index + 1, state.length)];
+    default:
+      return state;
+  }
+};
+
+const removeItem = (index) => {
+  return {
+    type: "REMOVE_ITEM",
+    index,
   };
 };
 
