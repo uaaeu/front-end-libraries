cc607086eaa2e794ad2779a5f0fb2518d301a4ac
uaaeu <yurtmusa@gmail.com>
2020-07-05 11:36:10 +0300

    Dispatch an Action Event

--- Redux/action.js
+++ Redux/action.js
@@ -5,3 +5,15 @@ const action = {
 
 // Define an Action Creator
 const actionCreator = () => action;
+
+// Dispatch an Action Event
+const store = Redux.createStore((state = { login: false }) => state);
+
+const loginAction = () => {
+  return {
+    type: "LOGIN",
+  };
+};
+
+// Dispatch the action
+store.dispatch(loginAction());
