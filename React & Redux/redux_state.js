c1dddc758ef59acaa2965f94f8b77087a3e4306f
uaaeu <yurtmusa@gmail.com>
2020-07-05 18:05:24 +0300

    Extract State Logic to Redux

---
+++ React & Redux/redux_state.js
@@ -0,0 +1,19 @@
+// Extract State Logic to Redux
+const ADD = "ADD";
+
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message,
+  };
+};
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.message];
+    default:
+      return state;
+  }
+};
+const store = Redux.createStore(messageReducer);
