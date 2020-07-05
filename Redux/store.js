acb69348e9fcc89450f4fb8baf7f385200b35cfb
uaaeu <yurtmusa@gmail.com>
2020-07-05 11:20:01 +0300

    Get State from the Redux Store

--- Redux/store.js
+++ Redux/store.js
@@ -1,7 +1,10 @@
-//Create a Redux Store
 const reducer = (state = 5) => {
   return state;
 };
 
 // Redux methods are available from a Redux object
+// Create a Redux Store
 const store = Redux.createStore(reducer);
+
+// Get State from the Redux Store
+const currentState = store.getState();
