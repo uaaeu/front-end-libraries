14667e580c15bb5c61037454bc0f752a528b7f95
uaaeu <yurtmusa@gmail.com>
2020-07-07 16:08:16 +0300

    getQuotes func created

--- Projects/Random Quote Machine/js/main.js
+++ Projects/Random Quote Machine/js/main.js
@@ -10,31 +10,47 @@ const inFrame = () => {
   }
 };
 
-const colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', '#472E32', '#BDBB99', '#77B1A9', '#73A857']
-let currentQuote = '', currentAuthor = '';
+const colors = [
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+];
+let currentQuote = "",
+  currentAuthor = "";
 const openURL = (url) => {
-    window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=0');
-}
-// /*
-//   Code by Gabriel Nunes
-//   Modified by Todd Chaffee to use Camper gist for JSON Quote data.
-// */
+  window.open(
+    url,
+    "Share",
+    "width=550, height=400, toolbar=0, scrollbars=1, location=0, statusbar=0, menubar=0, resizable=0"
+  );
+};
 
-// function getQuotes() {
-//   return $.ajax({
-//     headers: {
-//       Accept: "application/json"
-//     },
-//     url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
-//     success: function(jsonQuotes) {
-//       if (typeof jsonQuotes === 'string') {
-//         quotesData = JSON.parse(jsonQuotes);
-//         console.log('quotesData');
-//         console.log(quotesData);
-//       }
-//     }
-//   });
-// }
+const getQuotes = () => {
+  return $.ajax({
+    headers: {
+      Accept: "application/json",
+    },
+    url:
+      "https://raw.githubusercontent.com/uaaeu/front-end-libraries/master/Projects/Random%20Quote%20Machine/js/quotes.json",
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === "string") {
+        quotesData = JSON.parse(jsonQuotes);
+        console.log("quotesData");
+        console.log(quotesData);
+      }
+    },
+  });
+};
+
 
 // function getRandomQuote() {
 //   return quotesData.quotes[Math.floor(Math.random() * quotesData.quotes.length)];
